de2be5fc08479aee81d7a739fef8650cdf7ecc02
Maciej Kowalski <maciej33kowalski@gmail.com>
2014-03-23 20:36:49 +0100

    fix app.js and list all lists in Home#index

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,12 +2,12 @@ var app = angular.module("Masters", ["ngResource"]);
 
 app.factory("List", [
   "$resource", function($resource) {
-    return $resource("lists/:id", {id: "@id"}, {update: {method: "PUT"}});
+    return $resource("lists.json/:id", {id: "@id"}, {update: {method: "PUT"}});
   }
 ]);
 
 this.MainCtrl = [
-  "$scope", "Entry", function($scope, Entry) {
+  "$scope", "List", function($scope, List) {
     $scope.lists = List.query();
 
     $scope.addList = function() {
