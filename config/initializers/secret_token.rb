7a3572d6c8bbcfb07aa736d2b5fabc4a6762c03f
Maciej Kowalski <maciej33kowalski@gmail.com>
2014-03-23 18:44:09 +0100

    add all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Masters::Application.config.secret_key_base = 'c6c9c1cc620168a64e6bb3b99076d408dab125360fb25b9b7fb0cd6962952f779ecca21e2fae2ebfdb65a74d5093eac8cbfcf38187385bb321b731b4a10cca98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Masters::Application.config.session_store :cookie_store, key: '_masters_session'
