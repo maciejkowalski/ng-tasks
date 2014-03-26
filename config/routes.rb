07182ccc39d8776e0ec791479668651c5c8ffda9
Maciej Kowalski <maciej33kowalski@gmail.com>
2014-03-26 21:13:47 +0100

    use tasks as nested resources for lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Masters::Application.routes.draw do
+
   root to: "home#index"
 
-  resources :lists
+  resources :lists do
+    resources :tasks
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
