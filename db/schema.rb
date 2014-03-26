18e4915f18e0d90d44548d2b26bca74c28a48dfe
Maciej Kowalski <maciej33kowalski@gmail.com>
2014-03-26 21:37:48 +0100

    db/schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323185847) do
+ActiveRecord::Schema.define(version: 20140326201030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,5 +21,15 @@ ActiveRecord::Schema.define(version: 20140323185847) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "tasks", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "list_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tasks", ["list_id"], name: "index_tasks_on_list_id", using: :btree
 
 end
