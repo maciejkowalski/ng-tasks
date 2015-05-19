02779794218cf64f42f5c55c3278e22ea28b58ad
Maciej Kowalski <maciej33kowalski@gmail.com>
2015-05-19 14:23:45 +0200

    add Comment AR model, migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429212419) do
+ActiveRecord::Schema.define(version: 20150519122256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "task_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["task_id"], name: "index_comments_on_task_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "lists", force: true do |t|
     t.string   "name"
