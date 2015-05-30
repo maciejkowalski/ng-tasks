61c439b491f5495de639fe73ce7c67aec93504db
Maciej Kowalski <maciej33kowalski@gmail.com>
2015-05-30 13:04:16 +0200

    create attachment AR RoR model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519122256) do
+ActiveRecord::Schema.define(version: 20150530110310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attachments", force: true do |t|
+    t.string   "attachment"
+    t.integer  "task_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "attachments", ["task_id"], name: "index_attachments_on_task_id", using: :btree
+  add_index "attachments", ["user_id"], name: "index_attachments_on_user_id", using: :btree
 
   create_table "comments", force: true do |t|
     t.text     "content"
