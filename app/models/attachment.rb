b4ace7cc1196b0923cfc00aacce54e1888a6d29e
Maciej Kowalski <maciej33kowalski@gmail.com>
2015-05-30 16:51:02 +0200

    add missing helpers to attachment model

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,4 +3,12 @@ class Attachment < ActiveRecord::Base
   belongs_to :user
 
   mount_uploader :attachment, ::AttachmentUploader
+
+  def file_name
+    attachment.try(:file).try(:filename)
+  end
+
+  def url
+    attachment.try(:url)
+  end
 end
